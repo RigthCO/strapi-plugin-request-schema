068338781937b3e956b333b4da7081eeb32121d3
Bruno Ascenção <bruno.ascencao@ntweb.pt>
2025-10-06 17:33:51 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,3 +18,5 @@ export default {
 // Re-add the config.ts and export it (maybe im doing some bad programming not having it)
 // strapi.config.get("strapi-plugin-request-schema") this is retorning nothing and im not sure it should have the config the end user add's into the config/plugin file
 // the thing is, this is capable of accessing the config strapi.plugin("strapi-plugin-request-schema").config("bar") but im not sure if those configs are 2 different configs
+
+// Test
